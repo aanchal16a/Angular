55f2e937b1753a52e9377c8694ca4d6c0e10ebdf
samba <samba.pendela@gmail.com>
2017-12-05 20:38:09 +0530

    Variables

---
+++ Variables/variables.ts
@@ -0,0 +1,13 @@
+var var_one:string = "Angular5";
+var var_two:number= 100;
+var var_three:boolean=true;
+var var_four:any = "Data From DataBase Soon...!";
+var var_five:any[] = ["AngularJS","Angular5","NodeJS","MongoDB"];
+var var_six:number[] = [10,20,30,40,50];
+
+document.write("<h1>"+var_one+"</h1><br>");
+document.write("<h1>"+var_two+"</h1><br>");
+document.write("<h1>"+var_three+"</h1><br>");
+document.write("<h1>"+var_four+"</h1><br>");
+document.write("<h1>"+var_five+"</h1><br>");
+document.write("<h1>"+var_six+"</h1><br>");
